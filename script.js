31a870273473612180e62b31b36fc75071b36b8a
hory77 <zzcarrythzz@gmail.com>
2021-07-09 14:55:33 +0300

    added js file

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 let pushTaskBtn = document.getElementById("text__field-btn");
+let resetTextField = document.getElementById("text__field");
+let taskList = document.getElementById("wish__list-items");
 
 pushTaskBtn.onclick = function () {
-  let taskList = document.getElementById("wish__list-items");
-  taskList.insertAdjacentHTML(
-    `beforeend`,
-    `<div id='wish__list-item'>${
-      document.getElementById("text__field").value
-    }</div>`
-  );
+  if (document.querySelector(".wish__list input").value.length == 0) {
+  } else {
+    taskList.insertAdjacentHTML(
+      `beforeend`,
+      `<div id='wish__list-item'>${
+        document.getElementById("text__field").value
+      }</div>`
+    );
+  }
+
+  resetTextField.value = "";
+
+  let taskDone = document.querySelectorAll("#wish__list-item");
+  for (let i = 0; i < taskDone.length; i++) {
+    taskDone[i].onclick = function () {
+      this.classList.toggle("task__done");
+    };
+  }
 };
